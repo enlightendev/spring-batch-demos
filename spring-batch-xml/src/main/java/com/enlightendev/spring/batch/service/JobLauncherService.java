19a47c18bdbbfcd44666b4cc7aa98e846890df19
enlightendev <juan.lamadrid@gmail.com>
2015-04-27 11:17:46 -0400

    add runtime parameters to uniquely id each job run.

--- spring-batch-xml/src/main/java/com/enlightendev/spring/batch/service/JobLauncherService.java
+++ spring-batch-xml/src/main/java/com/enlightendev/spring/batch/service/JobLauncherService.java
@@ -1,15 +1,14 @@
 package com.enlightendev.spring.batch.service;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class JobLauncherService {
@@ -22,10 +21,10 @@ public class JobLauncherService {
 
     public void launchJob() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
 
-        JobParameters jobParameters = new JobParameters();
-        JobExecution execution = jobLauncher.run(job, jobParameters);
+        JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+        jobParametersBuilder.addDate("run-date",new Date());
+
+        JobExecution execution = jobLauncher.run(job, jobParametersBuilder.toJobParameters());
         System.out.println(execution.getExitStatus());
-
     }
-
 }
