70565d127c3afbabed8bb309231bae32bece951e
enlightendev <juan.lamadrid@gmail.com>
2015-05-01 11:06:07 -0400

    unused imports

--- spring-batch-xml/src/main/java/com/enlightendev/spring/batch/SpringBatchXmlApplication.java
+++ spring-batch-xml/src/main/java/com/enlightendev/spring/batch/SpringBatchXmlApplication.java
@@ -1,16 +1,12 @@
 package com.enlightendev.spring.batch;
 
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBatchXmlApplication {
 
-    public static void main(String[] args) throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+    public static void main(String[] args) {
         SpringApplication.run(SpringBatchXmlApplication.class, args);
     }
 
